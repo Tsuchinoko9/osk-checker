12ccfdcbdeedc68d534be9920e42676b31e95884
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-24 14:09:56 +0900

    マイページを仮作成
    ・usersのshowアクションのルーティングを設定
    ・userのコントローラーを作成(ターミナルで「rails g controller users」のコマンドを実行)
    ・userのコントローラーにshowアクションを定義
    ・userのshowアクションに対応するビューファイルを仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'favorites#index'
   resources :favorites, only: [:index]
+  resources :users, only: [:show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
