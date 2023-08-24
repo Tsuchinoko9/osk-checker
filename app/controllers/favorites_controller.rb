1e7102711d3150a94e0ffc69ffd2ba0656fa5ced
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-24 15:07:04 +0900

    favoritesのコントローラーを編集
    ・newとcreateのアクションを定義
    ・favoritesのストロングパラメーターを設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,25 @@
 class FavoritesController < ApplicationController
   def index
   end
+
+  def new
+    @favorite = Favorite.new
+  end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    if @favorite.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+
+private
+
+def favorite_params
+  params.require(:favorite).permit(:url, :name, :priority_order, :contents, :update_check)
+        .merge(user_id: current_user.id)
 end
